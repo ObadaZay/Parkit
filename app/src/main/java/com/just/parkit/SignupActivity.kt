ea8e65aa96dce584e16e78d943659b8198635289
ObadaZay <obada.zayadneh@gmail.com>
2020-04-22 21:25:33 +0300

    Initial commit

---
+++ app/src/main/java/com/just/parkit/SignupActivity.kt
@@ -0,0 +1,172 @@
+package com.just.parkit
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.just.parkit.SplashActivity.Companion.familyName
+import com.just.parkit.SplashActivity.Companion.fatherName
+import com.just.parkit.SplashActivity.Companion.firstName
+import com.just.parkit.SplashActivity.Companion.password
+import com.just.parkit.SplashActivity.Companion.prefs
+import com.just.parkit.SplashActivity.Companion.prefsFileName
+import kotlinx.android.synthetic.main.activity_signup.*
+
+class SignupActivity : AppCompatActivity() {
+
+    var check1: Int? = 2 //0 means missing data, 1 means successful, 2 means not changed value
+    var check2: Int? = 2 //0 means missing data, 1 means successful, 2 means not changed value
+    var check3: Int? = 2 //0 means missing data, 1 means successful, 2 means not changed value
+    var check4: Int? = 2 //0 means missing data, 1 means successful, 2 means not changed value
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        //get sharedpref instance
+        prefs = this.getSharedPreferences(prefsFileName, Context.MODE_PRIVATE)
+
+        //when next button is clicked
+        bu_signup_next.setOnClickListener {
+
+            //call this function which checks for errors of user input
+            checkUser()
+
+            //handle the errors
+            if (check1 == 0 || check2 == 0 || check3 == 0 || check4 == 0) {
+                Toast.makeText(applicationContext, "Please double check that you filled all the fields correctly ^ ^", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            else if (check1 == 1 && check2 == 1 && check3 == 1 && check4 == 1) {
+                //call saveUser() Function to save the user unitl registration is complete
+                saveUser()
+            }
+            else {
+                Toast.makeText(applicationContext, "Register Error, please try again later :(", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+        }
+
+
+    }
+
+
+    fun checkUser() {
+
+        if (et_signup_password.text.isNullOrEmpty()) {
+            et_signup_password.error = "You can't leave this field empty"
+            et_signup_password.requestFocus()
+            check1 = 0
+        }
+        else {
+            if (et_signup_password.text.toString().length < 6){
+                et_signup_password.error = "Your password has to be at least 6 characters"
+                et_signup_password.requestFocus()
+                check1 = 0
+            }
+            else{
+                if (et_signup_password.text!!.contains(" ")){
+                    et_signup_password.error = "Your password cannot contain any spaces"
+                    et_signup_password.requestFocus()
+                    check1 = 0
+                }
+                else {
+                    //success
+                    check1 = 1
+                }
+            }
+        }
+
+        if (et_family_name.text.isNullOrEmpty()) {
+            et_family_name.error = "You can't leave this field empty"
+            et_family_name.requestFocus()
+            check2 = 0
+        }
+        else {
+            if (et_family_name.text.toString().length == 1){
+                et_family_name.error = "Your family name is too short, please use the real one"
+                et_family_name.requestFocus()
+                check2 = 0
+            }
+            else{
+                if (et_family_name.text!!.contains(" ")){
+                    et_family_name.error = "Your family name cannot contain any spaces"
+                    et_family_name.requestFocus()
+                    check2 = 0
+                }
+                else {
+                    //success
+                    check2 = 1
+                }
+            }
+        }
+
+        if (et_father_name.text.isNullOrEmpty()) {
+            et_father_name.error = "You can't leave this field empty"
+            et_father_name.requestFocus()
+            check3 = 0
+        }
+        else {
+            if (et_father_name.text.toString().length == 1){
+                et_father_name.error = "Your father name is too short, please use the real one"
+                et_father_name.requestFocus()
+                check3 = 0
+            }
+            else{
+                if (et_father_name.text!!.contains(" ")){
+                    et_father_name.error = "Your father name cannot contain any spaces"
+                    et_father_name.requestFocus()
+                    check3 = 0
+                }
+                else {
+                    //success
+                    check3 = 1
+                }
+            }
+        }
+
+        if (et_first_name.text.isNullOrEmpty()) {
+            et_first_name.error = "You can't leave this field empty"
+            et_first_name.requestFocus()
+            check4 = 0
+        }
+        else {
+            if (et_first_name.text.toString().length == 1){
+                et_first_name.error = "Your first name is too short, please use your real name"
+                et_first_name.requestFocus()
+                check4 = 0
+            }
+            else{
+                if (et_first_name.text!!.contains(" ")){
+                    et_first_name.error = "Your first name cannot contain any spaces"
+                    et_first_name.requestFocus()
+                    check4 = 0
+                }
+                else {
+                    //success
+                    check4 = 1
+                }
+            }
+        }
+    }
+
+
+    //this function is used to save the user data in shared preferences after check is complete
+    fun saveUser() {
+
+        //save the data of these fields at these variables
+        firstName = et_first_name.text.toString()
+        fatherName = et_father_name.text.toString()
+        familyName = et_family_name.text.toString()
+        password = et_signup_password.text.toString()
+
+        //put the data inside shared pref until the user successfully complete his registration
+        prefs?.edit()?.putString("firstName", firstName)?.apply()
+        prefs?.edit()?.putString("fatherName", fatherName)?.apply()
+        prefs?.edit()?.putString("familyName", familyName)?.apply()
+        prefs?.edit()?.putString("password", password)?.apply()
+
+    }
+
+}
---
+++ app/src/main/java/com/just/parkit/SplashActivity.kt
@@ -0,0 +1,124 @@
+package com.just.parkit
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.lang.Exception
+import android.content.SharedPreferences
+
+
+class SplashActivity : AppCompatActivity() {
+
+    companion object {
+
+        //variables for the shared preferences
+        val prefsFileName = "com.just.parkit.prefs"
+        var prefs: SharedPreferences? = null
+
+        // define var for signup sucess
+        //0 means fail or signed out, 1 means success
+        //2 means he entered phone and waiting for verification message, 3 means he is logged in
+        var signupsuccess: String? = null
+
+        // define var for signup result serverside
+        //0 means fail, 1 means success
+        //2 means user phone exists
+        var signupresult: String? = null
+
+
+        //user firstName
+        var firstName = "null"
+        //user fatherName
+        var fatherName = "null"
+        //user familyName
+        var familyName = "null"
+        //user password
+        var password = "null"
+        //user phonenumber
+        var phone = "null"
+    }
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+        //the font
+        //val typeface = Typeface.createFromAsset(assets, "Arslan-Wessam-B.ttf")
+
+
+
+        //sharedpreferences get
+        prefs = this.getSharedPreferences(prefsFileName, MODE_PRIVATE)
+        signupresult = prefs!!.getString("signupresult", "")
+        signupsuccess = prefs!!.getString("signupsuccess", "")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        //check if user signup is successful if its not take him through the signup form else take him to the main activity
+
+        if (signupresult.isNullOrEmpty() || signupresult == "0" || signupresult == "2" || signupsuccess.isNullOrEmpty()) {
+
+            //the delay on splash and goto first activity
+            val background = object : Thread() {
+                override fun run() {
+                    try {
+                        Thread.sleep(4000)
+
+                        //todo it was first activity
+                        val intent = Intent(baseContext, MainActivity::class.java)
+                        startActivity(intent)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+            background.start()
+
+        }
+
+        else if (signupsuccess == "2") {
+
+            //the delay on splash and goto auth activity
+            val background = object : Thread() {
+                override fun run() {
+                    try {
+                        Thread.sleep(2000)
+
+                        //todo turkish scholarship
+                        //todo this was phoneauth activity
+                        val intent = Intent(baseContext, MainActivity::class.java)
+                        startActivity(intent)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+            background.start()
+
+        }
+
+        else {
+
+            //the delay on splash and goto main activity
+            val background = object : Thread() {
+                override fun run() {
+                    try {
+                        Thread.sleep(2000)
+
+                        val intent = Intent(baseContext, MainActivity::class.java)
+                        startActivity(intent)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+            background.start()
+        }
+    }
+}
